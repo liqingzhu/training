00c66d5049af57b301a902eb5e84e89ba2e44996
liqingzhu20009 <liqingzhu2009@sina.com>
2016-06-20 11:43:38 +0800

    添加Java项目文件

---
+++ Code/01JAVA/training/src/com/lee/object/Cat.java
@@ -0,0 +1,7 @@
+package com.lee.object;
+
+public class Cat extends Animal{
+	public void eat(){
+		System.out.println("√®≥‘”„!");
+	}
+}
---
+++ Code/01JAVA/training/src/com/lee/object/Dog.java
@@ -0,0 +1,7 @@
+package com.lee.object;
+
+public class Dog extends Animal{
+	public void eat(){
+		System.out.println("π∑≥‘»‚!");
+	}
+}
